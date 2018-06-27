6c202f6d13c5d689f9d5a93a084b45e9971ee22d
Marius Bakkeli <marius@contesto.no>
2018-06-27 15:17:36 +0200

    testing authentcation

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace titanapi_dotnetcore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] LoginModel model)
+        {
+            try
+            {
+                if (model.Username.ToLower().Equals("error"))
+                {
+                    throw new Exception("Some error occured");
+                }
+
+
+                if (!model.Username.ToLower().Equals("ida@kolibrimedical.no"))
+                {
+                    return Unauthorized();
+                }
+                
+
+                var user = new AppAuthUser
+                    {
+                        username = model.Username,
+                        bearerToken = "token",
+                        isAuthenticated = true,
+                    };
+
+                return Ok(user);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+
+        }
+    }
+}
---
+++ Model/AppAuthUser.cs
@@ -0,0 +1,6 @@
+public class AppAuthUser
+{
+    public string username { get; set; }
+    public string bearerToken { get; set; }
+    public bool isAuthenticated { get; set; }
+}
